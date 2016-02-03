9821811cfc8a74e62a85b484d651091eaffa9931
multivibrator <karl.svantorp@gmail.com>
2016-02-03 13:05:11 +0100

    prefetcers with list of previous requests

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -6,11 +6,31 @@
 
 #include "interface.hh"
 
+struct List {
+    List();
+    request * first, * last;
+};
+
+List::List() : first(nullptr), last(nullptr){}
+
+struct Request {
+    Request(Addr addr, bool ms, Request *prv)
+    bool miss;
+    Addr adress;
+    int strideToPrev;
+    request * next, * prev;
+};
+
+Request::Request(Addr addr, bool ms, Request *prv) : adress(addr), miss(ms), prev(prv), next(nullptr){}
+
+static List * list;
+
 
 void prefetch_init(void)
 {
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
+    list = new List()
 
     DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
 }
@@ -18,13 +38,26 @@ void prefetch_init(void)
 void prefetch_access(AccessStat stat)
 {
     /* pf_addr is now an address within the _next_ cache block */
-    Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
+    Request req = new Request(stat.mem_addr,stat.miss,list.last);
+    
+    if (!(list->first)){
+        list->first = &req;
+        list->last = &req;
+    } else {
+        list->last->next = &req;
+        list->last = &req;
+        list->last.strideToPrev = list->last->prev->adress - list->last->adress
+    }
+    
+    Addr pf_addr = stat.mem_addr + list->last->strideToPrev;
+    
+    
 
     /*
      * Issue a prefetch request if a demand miss occured,
      * and the block is not already in cache.
      */
-    if (stat.miss && !in_cache(pf_addr)) {
+    if (stat.miss && !in_cache(pf_addr) && in_mshr_queue(pf_addr)) {
         issue_prefetch(pf_addr);
     }
 }
