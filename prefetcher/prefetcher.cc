11cd959b5a46a4ad5306cf8decce8266a08bb95e
Audun Sutterud <audunsu@stud.ntnu.no>
2016-02-10 13:36:07 +0100

    Clean up the template prefetcher.cc
    Cleaned out the example code from the functions and added a descriptive
    comment at the top of the file.

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -1,7 +1,9 @@
 /*
- * A sample prefetcher which does sequential one-block lookahead.
- * This means that the prefetcher fetches the next block _after_ the one that
- * was just accessed. It also ignores requests to blocks already in the cache.
+ * A prefetcher based on the approach "Stride Directed Prefetching (SDP)"
+ * described in the doctoral thesis:
+ *     Reducing Memory Latency by Improving Resource Utilization
+ *     by Marius Grannæs
+ *     http://www.idi.ntnu.no/research/doctor_theses/grannas.pdf
  */
 
 #include "interface.hh"
@@ -9,28 +11,16 @@
 
 void prefetch_init(void)
 {
-    /* Called before any calls to prefetch_access. */
-    /* This is the place to initialize data structures. */
+    DPRINTF(HWPrefetch, "Initialized stride-directed prefetcher\n");
 
-    DPRINTF(HWPrefetch, "Initialized sequential-on-access prefetcher\n");
+	// TODO: Initialize prefetcher.
 }
 
 void prefetch_access(AccessStat stat)
 {
-    /* pf_addr is now an address within the _next_ cache block */
-    Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
-
-    /*
-     * Issue a prefetch request if a demand miss occured,
-     * and the block is not already in cache.
-     */
-    if (stat.miss && !in_cache(pf_addr)) {
-        issue_prefetch(pf_addr);
-    }
+	// TODO: Implement.
 }
 
 void prefetch_complete(Addr addr) {
-    /*
-     * Called when a block requested by the prefetcher has been loaded.
-     */
+	// TODO: Implement.
 }
